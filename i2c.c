a9587f1eb4d94d14ef12b6f33e37339b478d64d7
Artem Kliminskyi <graphene64@gmail.com>
2024-03-11 14:35:50 +0100

    Add code for I2C data transfer.

---
+++ i2c.c
@@ -0,0 +1,98 @@
+#include "i2c.h"
+#include <util/delay.h>
+
+const double I2C_PERIOD = 10.0; // In microseconds. 100 kHz
+
+static void send_bit(char bit) {
+    if (bit) {
+        SDA_PORT |= SDA_PORT_BIT;
+    }
+    else {
+        SDA_PORT &= ~SDA_PORT_BIT;
+    }
+    _delay_us(I2C_PERIOD / 4.0);
+    SCL_PORT |= SCL_PORT_BIT;
+    _delay_us(I2C_PERIOD / 4.0);
+    SCL_PORT &= ~SCL_PORT_BIT;
+    SDA_PORT &= ~SDA_PORT_BIT;
+    _delay_us(I2C_PERIOD / 2.0);
+}
+
+static char ack() {
+    _delay_us(I2C_PERIOD / 4.0);
+    SCL_PORT |= SCL_PORT_BIT;
+    _delay_us(I2C_PERIOD / 4.0);
+
+    SDA_DDR &= ~SDA_PORT_BIT;
+    char result = SDA_PORT & SDA_PORT_BIT;
+    SDA_DDR |= SDA_PORT_BIT;
+
+    SCL_PORT &= ~SCL_PORT_BIT;
+    _delay_us(I2C_PERIOD / 2.0);
+
+    return result;
+}
+
+void start_condition(void) {
+    SDA_PORT |= SDA_PORT_BIT;
+    SCL_PORT |= SCL_PORT_BIT;
+
+    _delay_us(I2C_PERIOD);
+
+    SDA_PORT &= ~SDA_PORT_BIT;
+
+    _delay_us(I2C_PERIOD / 2.0);
+
+    SCL_PORT &= ~SCL_PORT_BIT;
+
+    _delay_us(I2C_PERIOD);
+}
+
+static void stop_condition(void) {
+    SDA_PORT &= ~SDA_PORT_BIT;
+    SCL_PORT &= ~SCL_PORT_BIT;
+
+    _delay_us(I2C_PERIOD);
+
+    SCL_PORT |= SCL_PORT_BIT;
+    
+    _delay_us(I2C_PERIOD / 2.0);
+
+    SDA_PORT |= SDA_PORT_BIT;
+
+    _delay_us(I2C_PERIOD);
+}
+
+static void send_address(char address) {
+    // Send the 7-bit address bits.
+    for (char i = 0; i < 7; i++) {
+        send_bit(address & 0b01000000);
+        address <<= 1;
+    }
+
+    // R/W bit.
+    send_bit(0);
+    
+    // ACK bit.
+    ack();
+}
+
+void i2c_start_transmission(unsigned char slave_address) {
+    SDA_DDR |= SDA_PORT_BIT;
+    SCL_DDR |= SCL_PORT_BIT;
+    start_condition();
+    send_address(slave_address);
+    _delay_us(I2C_PERIOD);
+}
+
+void i2c_end_transmission(void) {
+    stop_condition();
+}
+
+void i2c_send_byte(char byte) {
+    for (char i = 0; i < 8; i++) {
+        send_bit(byte & 0b10000000);
+        byte <<= 1;
+    }
+    ack();
+}
---
+++ i2c.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <avr/io.h>
+
+#define SDA_PORT PORTC
+#define SDA_DDR DDRC
+#define SCL_PORT PORTC
+#define SCL_DDR DDRC
+#define SDA_PORT_BIT (0b00000001 << 4)
+#define SCL_PORT_BIT (0b00000001 << 5)
+
+void i2c_start_transmission(unsigned char slave_address);
+void i2c_end_transmission(void);
+
+void i2c_send_byte(char byte);
